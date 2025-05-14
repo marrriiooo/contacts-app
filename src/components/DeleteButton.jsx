83f8461ecd4517471baf70b88cda61478b290071
marrriiooo <mariosenawati@gmail.com>
2025-05-14 18:49:38 +0800

    memperbarui icon

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { FiDelete } from "react-icons/fi";
 
 function DeleteButton({ id, onDelete }) {
   return (
-    <div>
-      <button className="contact-item__delete" onClick={() => onDelete(id)}>
-        x
-      </button>
-    </div>
+    <button className="contact-item__delete" onClick={() => onDelete(id)}>
+      <FiDelete />
+    </button>
   );
 }
+
+DeleteButton.propTypes = {
+  id: PropTypes.number.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default DeleteButton;
